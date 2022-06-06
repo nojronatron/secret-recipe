ec0da1818d63956c2982dedb568f9cb1e319e50d
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-06 14:34:07 -0700

    initialized project

---
+++ src/main/java/com/JonsSecretReceipe/SecretRecipe/SecretRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.JonsSecretReceipe.SecretRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretRecipeApplication.class, args);
+	}
+
+}
