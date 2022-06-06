5752a66041d17ab02abf0f136a9ed5669867a7e3
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-06 15:57:54 -0700

    updated controllers and added template html

---
+++ src/main/java/com/JonsSecretReceipe/SecretRecipe/AuthController.java
@@ -0,0 +1,28 @@
+package com.JonsSecretReceipe.SecretRecipe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class AuthController {
+    @GetMapping("/signup") public String getSignupPage() { return "signup";}
+
+    @PostMapping("/signup") public RedirectView addUser(Model springModel, String _username, String _password) {
+        // TODO: implement signup functionality
+
+        // for now just push the user to the login page
+        return new RedirectView("login");
+    }
+
+    @GetMapping("/login") public String getLoginPage() {return "login";}
+
+    @PostMapping("/login") public RedirectView getAuthUser(String _username, String _password){
+        // TODO: implement login verification and protect access to authorized users only
+
+        // for now just return a simple page to verify paths work
+        return new RedirectView("protectedpage");
+    }
+}
