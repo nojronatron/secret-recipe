c3bc179caa955a578cc6300e1224d879f34ca7dd
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-06 20:54:50 -0700

    completed wiring-up authentication path.

--- src/main/java/com/JonsSecretReceipe/SecretRecipe/AuthController.java
+++ src/main/java/com/JonsSecretReceipe/SecretRecipe/AuthController.java
@@ -1,5 +1,6 @@
 package com.JonsSecretReceipe.SecretRecipe;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +10,34 @@ import org.mindrot.jbcrypt.BCrypt;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    SiteUserRepository siteUserRepository;
+
     @GetMapping("/signup") public String getSignupPage() { return "signup";}
 
     // acquire site user information submitted by the form, insert user into db, then send user to the login page
-    @PostMapping("/signup") public RedirectView addUser(Model springModel, String _username, String _password) {
-        String passwordHash = BCrypt.hashpw(_password, BCrypt.gensalt(11));
-        // for now just push the user to the login page
+    @PostMapping("/signup") public RedirectView postNewUser(Model springModel, String username, String password) {
+        String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt(11));
+        SiteUser siteUser = new SiteUser(username, passwordHash);
+        siteUserRepository.save(siteUser);
+
+        // send the user to the login page
         return new RedirectView("login");
     }
 
     @GetMapping("/login") public String getLoginPage() {return "login";}
 
     // get the site user and validate login information then send to main web page if auth passes
-    @PostMapping("/login") public RedirectView getAuthUser(String _username, String _password){
+    @PostMapping("/login") public RedirectView postAuthUser(String username, String password){
+        SiteUser siteUser = siteUserRepository.findByUsername(username);
 
+        if ((siteUser == null) || (!BCrypt.checkpw(password, siteUser.password))) {
+            return new RedirectView("/login");
+        }
         // for now just return a simple page to verify paths work
-        return new RedirectView("main");
+        return new RedirectView("/protectedpage");
     }
+
+    @GetMapping("/protectedpage") public String getProtectedHomepage() { return "protectedpage"; }
 }
